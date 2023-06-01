6c0abc622d5b343f485bfef68ced56218f4c4eb2
Dev01 <guilhermebrasil.acs@outlook.com>
2023-06-01 11:10:55 -0300

    Adding Users

--- src/main/java/com/ACS/ACSproject/controller/UserController.java
+++ src/main/java/com/ACS/ACSproject/controller/UserController.java
@@ -1,13 +1,53 @@
 package com.ACS.ACSproject.controller;
 
+import com.ACS.ACSproject.dto.UserDTO;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping("/user")
 public class UserController {
-    @GetMapping("/testeACS")
-    public String getMensagem(){
-        return "Spring boot is working";
+    public static List<UserDTO> usuarios = new ArrayList<UserDTO>();
+
+    @PostConstruct
+    public void initiateList(){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNome("Eduardo");
+        userDTO.setCpf("123");
+        userDTO.setEndereco("Rua A");
+        userDTO.setEmail("eduardo@gmail.com");
+        userDTO.setTelefone("1234-3454");
+        userDTO.setDataCadastro(LocalDateTime.now());
+
+        UserDTO userDTO2 = new UserDTO();
+        userDTO2.setNome("Luiz");
+        userDTO2.setCpf("456");
+        userDTO2.setEndereco("Rua B");
+        userDTO2.setEmail("luiz@gmail.com");
+        userDTO2.setTelefone("3234-6878");
+        userDTO2.setDataCadastro(LocalDateTime.now());
+
+        UserDTO userDTO3 = new UserDTO();
+        userDTO3.setNome("Bruna");
+        userDTO3.setCpf("789");
+        userDTO3.setEndereco("Rua C");
+        userDTO3.setEmail("bruna@gmail.com");
+        userDTO3.setTelefone("2987-9076");
+        userDTO3.setDataCadastro(LocalDateTime.now());
+
+        usuarios.add(userDTO);
+        usuarios.add(userDTO2);
+        usuarios.add(userDTO3);
+    }
+
+    @GetMapping
+    public List<UserDTO> getUsers(){
+        return usuarios;
     }
 }
