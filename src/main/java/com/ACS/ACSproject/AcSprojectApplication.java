af20972ea7d712e63524f6f2811c25ea98c1ef26
Dev01 <guilhermebrasil.acs@outlook.com>
2023-06-01 09:51:06 -0300

    Initial commit

---
+++ src/main/java/com/ACS/ACSproject/AcSprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ACS.ACSproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcSprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcSprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ACS/ACSproject/AcSprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ACS.ACSproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcSprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
